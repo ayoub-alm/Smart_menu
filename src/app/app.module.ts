d9d6c098f6ccc0adbe6959dd348bb634c73b82bc
ayoub_alm <alayoub33@gmail.com>
2023-06-29 06:45:33 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CategoriesSectionComponent } from './categories-section/categories-section.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { TopVenteSectionComponent } from './top-vente-section/top-vente-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    NavBarComponent,
+    CategoriesSectionComponent,
+    HeaderSectionComponent,
+    TopVenteSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/top-vente-section/top-vente-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-vente-section',
+  templateUrl: './top-vente-section.component.html',
+  styleUrls: ['./top-vente-section.component.css']
+})
+export class TopVenteSectionComponent {
+
+}
